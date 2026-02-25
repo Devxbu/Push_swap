92d85963c1936c5a540c68990827001c7decf31f
devxbu <bahri.official@protonmail.com>
2026-02-25 20:37:33 +0300

    Mode Parser

--- main.c
+++ main.c
@@ -1,3 +1,11 @@
 #include "push_swap.h"
+#include <stdio.h>
 
-int main(int argc, char **argv);
+int main(int argc, char **argv) {
+  t_mode mode;
+
+  if (argc < 2)
+    return (0);
+
+  mode = parse_mode(&argc, &argv);
+}
--- parsing/parsing_mode.c
+++ parsing/parsing_mode.c
@@ -1,3 +1,37 @@
 #include "../push_swap.h"
 
-t_mode parse_mode(int *argc, char ***argv);
+static void exit_err() {
+  write(2, "Error\n", 6);
+  exit(1);
+}
+
+static int ft_strcmp(const char *s1, const char *s2) {
+  while (*s1 && (*s1 == *s2)) {
+    s1++;
+    s2++;
+  }
+  return ((unsigned char)*s1 - (unsigned char)*s2);
+}
+
+t_mode parse_mode(int *argc, char ***argv) {
+  t_mode mode;
+
+  mode = ADAPTIVE;
+  if (*argc > 1 && (*argv)[1][0] == '-' && (*argv)[1][1] == '-') {
+    if (ft_strcmp((*argv)[1], "--simple") == 0)
+      mode = SIMPLE;
+    else if (ft_strcmp((*argv)[1], "--medium") == 0)
+      mode = MEDIUM;
+    else if (ft_strcmp((*argv)[1], "--complex") == 0)
+      mode = COMPLEX;
+    else if (ft_strcmp((*argv)[1], "--adaptive") == 0)
+      mode = ADAPTIVE;
+    else
+      exit_err();
+    (*argv)++;
+    (*argc)--;
+    if (*argc < 2)
+      exit_err();
+  }
+  return (mode);
+}
