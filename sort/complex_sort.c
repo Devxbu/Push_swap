bf8b7aa1f2f50362e3facb0155c87866859356c4
Mehran Elmbaz <melmbaz@k1m21s01.42istanbul.com.tr>
2026-03-10 17:03:50 +0300

    completed the complex sort(radix)

--- sort/complex_sort.c
+++ sort/complex_sort.c
@@ -6,7 +6,7 @@
 /*   By: melmbaz <melmbaz@student.42istanbul.com.tr>+#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/02/26 20:01:02 by melmbaz           #+#    #+#             */
-/*   Updated: 2026/02/27 01:05:46 by melmbaz          ###   ########.fr       */
+/*   Updated: 2026/03/10 16:26:54 by melmbaz          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,4 +14,24 @@
 
 void	complex_sort(t_list **a, int size)
 {
+	int		i;
+	int		bit_amount;
+	t_list	*b;
+
+	b = NULL;
+	bit_amount = 0;
+	while ((1 << bit_amount) < size)
+	{
+		i = 0;
+		while (i++ < size)
+		{
+			if (((*a)->content >> bit_amount) & 1)
+				ra(a);
+			else
+				pb(a, &b);
+		}
+		while (b)
+			pa(a, &b);
+		bit_amount++;
+	}
 }
